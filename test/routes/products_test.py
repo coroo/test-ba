22412240844cd5cafbdec815ec4337fb42d8d2d0
coroo <coroo.wicaksono@gmail.com>
2020-11-05 23:08:41 +0700

    :wrench: Fix unit test

--- test/routes/products_test.py
+++ test/routes/products_test.py
@@ -40,141 +40,169 @@ class TestProducts():
             f"{session_data['token_type']} {session_data['access_token']}"}
         self.headers = headers
 
-        # READ FIRST PRODUCT CATEGORY
-        TestProductCategories.test_create(self)
-        response = client.get(settings.API_PREFIX+"/product_categories/")
-        assert response.status_code == 200, response.text
-        category_data = response.json()
-        assert category_data[0]["id"] is not None
-        self.category_id = category_data[0]["id"]
-
-        # READ FIRST PRODUCT DETAIL
-        TestProductDetails.test_create(self)
-        response = client.get(settings.API_PREFIX+"/product_details/")
-        assert response.status_code == 200, response.text
-        detail_data = response.json()
-        assert detail_data[0]["id"] is not None
-        self.detail_id = detail_data[0]["id"]
-
-        # READ FIRST PRODUCT INSURANCE TYPE
-        TestProductInsuranceTypes.test_create(self)
-        response = client.get(settings.API_PREFIX+"/product_insurance_types/")
-        assert response.status_code == 200, response.text
-        insurance_type_data = response.json()
-        assert insurance_type_data[0]["id"] is not None
-        self.insurance_type_id = insurance_type_data[0]["id"]
-
         # NEGATIVE TEST
         self.wrong_id = 912093018209302910
 
-    def test_create(self):
+    @pytest.fixture()
+    def setup_cache(self, request):
+        # READ FIRST PRODUCT CATEGORY
+        c_category_id = request.config.cache.get("c_category_id", None)
+        if c_category_id is None:
+            TestProductCategories.test_create(self)
+            response = client.get(settings.API_PREFIX+"/product_categories/")
+            assert response.status_code == 200, response.text
+            category_data = response.json()
+            assert category_data[0]["id"] is not None
+            category_id = category_data[0]["id"]
+            request.config.cache.set("c_category_id", category_id)
+
+        # READ FIRST PRODUCT DETAIL
+        c_detail_id = request.config.cache.get("c_detail_id", None)
+        if c_detail_id is None:
+            TestProductDetails.test_create(self)
+            response = client.get(settings.API_PREFIX+"/product_details/")
+            assert response.status_code == 200, response.text
+            detail_data = response.json()
+            assert detail_data[0]["id"] is not None
+            detail_id = detail_data[0]["id"]
+            request.config.cache.set("c_insurance_type_id", detail_id)
+
+        # READ FIRST PRODUCT INSURANCE TYPE
+        c_insurance_type_id = request.config.cache.get(
+            "c_insurance_type_id", None)
+        if c_insurance_type_id is None:
+            TestProductInsuranceTypes.test_create(self)
+            response = client.get(
+                settings.API_PREFIX+"/product_insurance_types/")
+            assert response.status_code == 200, response.text
+            insurance_type_data = response.json()
+            assert insurance_type_data[0]["id"] is not None
+            insurance_type_id = insurance_type_data[0]["id"]
+            request.config.cache.set("c_insurance_type_id", insurance_type_id)
+
+    def test_create(self, request, setup_cache):
+        category_id = request.config.cache.get("c_category_id", None)
+        detail_id = request.config.cache.get("c_detail_id", None)
+        insurance_type_id = request.config.cache.get(
+            "c_insurance_type_id", None)
+
         response = client.post(
             settings.API_PREFIX+local_prefix,
             headers=self.headers,
             json={"slug": fake_slug,
                   "name": fake_name,
-                  "category_id": self.category_id,
-                  "insurance_type_id": self.insurance_type_id,
-                  "detail_id": self.detail_id,
+                  "category_id": category_id,
+                  "insurance_type_id": insurance_type_id,
+                  "detail_id": detail_id,
                   "premium_type": fake_premium_type},
         )
         assert response.status_code == 200, response.text
 
-    # def test_get(self):
-    #     # PREPARATION GET ID
-    #     response = client.get(
-    #         settings.API_PREFIX+local_prefix,
-    #         headers=self.headers,
-    #     )
-    #     assert response.status_code == 200, response.text
-    #     data = response.json()
-    #     assert data[0]['id'] is not None
-    #     assert "id" in data[0]
-    #     self.id_test = data[0]['id']
+    def test_get(self):
+        # PREPARATION GET ID
+        response = client.get(
+            settings.API_PREFIX+local_prefix,
+            headers=self.headers,
+        )
+        assert response.status_code == 200, response.text
+        data = response.json()
+        assert data[0]['id'] is not None
+        assert "id" in data[0]
+        self.id_test = data[0]['id']
 
-    #     response = client.get(settings.API_PREFIX+local_prefix,
-    #                           headers=self.headers,)
-    #     assert response.status_code == 200
+        response = client.get(settings.API_PREFIX+local_prefix,
+                              headers=self.headers,)
+        assert response.status_code == 200
 
-    #     response = client.get(
-    #         settings.API_PREFIX+local_prefix+str(data[0]['id']),
-    #         headers=self.headers,)
-    #     assert response.status_code == 200
+        response = client.get(
+            settings.API_PREFIX+local_prefix+str(data[0]['id']),
+            headers=self.headers,)
+        assert response.status_code == 200
 
-    # def test_update(self):
-    #     # PREPARATION GET ID
-    #     response = client.get(
-    #         settings.API_PREFIX+local_prefix,
-    #         headers=self.headers,
-    #     )
-    #     assert response.status_code == 200, response.text
-    #     data = response.json()
-    #     assert data[0]['id'] is not None
-    #     assert "id" in data[0]
-    #     self.id_test = data[0]['id']
+    def test_update(self, request, setup_cache):
+        category_id = request.config.cache.get("c_category_id", None)
+        detail_id = request.config.cache.get("c_detail_id", None)
+        insurance_type_id = request.config.cache.get(
+            "c_insurance_type_id", None)
 
-    #     response = client.get(settings.API_PREFIX+local_prefix,
-    #                           headers=self.headers,)
-    #     assert response.status_code == 200
+        # PREPARATION GET ID
+        response = client.get(
+            settings.API_PREFIX+local_prefix,
+            headers=self.headers,
+        )
+        assert response.status_code == 200, response.text
+        data = response.json()
+        assert data[0]['id'] is not None
+        assert "id" in data[0]
+        self.id_test = data[0]['id']
 
-    #     response = client.put(
-    #         f"{settings.API_PREFIX}{local_prefix}{self.id_test}",
-    #         json={"slug": fake_slug_2,
-    #               "name": fake_name,
-    #               "category_id": self.category_id,
-    #               "insurance_type_id": self.insurance_type_id,
-    #               "detail_id": self.detail_id,
-    #               "premium_type": fake_premium_type},
-    #     )
-    #     assert response.status_code == 200, response.text
-    #     data = response.json()
-    #     assert data["slug"] != fake_slug
-    #     assert data["slug"] == fake_slug_2
+        response = client.get(settings.API_PREFIX+local_prefix,
+                              headers=self.headers,)
+        assert response.status_code == 200
 
-    # def test_delete(self):
-    #     # PREPARATION GET ID
-    #     response = client.get(
-    #         settings.API_PREFIX+local_prefix,
-    #         headers=self.headers,
-    #     )
-    #     assert response.status_code == 200, response.text
-    #     data = response.json()
-    #     assert data[0]['id'] is not None
-    #     assert "id" in data[0]
-    #     self.id_test = data[0]['id']
+        response = client.put(
+            f"{settings.API_PREFIX}{local_prefix}{self.id_test}",
+            json={"slug": fake_slug_2,
+                  "name": fake_name,
+                  "category_id": category_id,
+                  "insurance_type_id": insurance_type_id,
+                  "detail_id": detail_id,
+                  "premium_type": fake_premium_type},
+        )
+        assert response.status_code == 200, response.text
+        data = response.json()
+        assert data["slug"] != fake_slug
+        assert data["slug"] == fake_slug_2
 
-    #     response = client.get(settings.API_PREFIX+local_prefix,
-    #                           headers=self.headers,)
-    #     assert response.status_code == 200
+    def test_delete(self):
+        # PREPARATION GET ID
+        response = client.get(
+            settings.API_PREFIX+local_prefix,
+            headers=self.headers,
+        )
+        assert response.status_code == 200, response.text
+        data = response.json()
+        assert data[0]['id'] is not None
+        assert "id" in data[0]
+        self.id_test = data[0]['id']
 
-    #     response = client.delete(
-    #         f"{settings.API_PREFIX}{local_prefix}",
-    #         json={"id": self.id_test},
-    #     )
-    #     assert response.status_code == 200
+        response = client.get(settings.API_PREFIX+local_prefix,
+                              headers=self.headers,)
+        assert response.status_code == 200
 
-    # # ============ NEGATIVE TEST ============
-    # def test_negative_get(self):
-    #     response = client.get(f"{settings.API_PREFIX}{local_prefix}" +
-    #                           f"{self.wrong_id}",
-    #                           headers=self.headers,)
-    #     assert response.status_code == 404
+        response = client.delete(
+            f"{settings.API_PREFIX}{local_prefix}",
+            json={"id": self.id_test},
+        )
+        assert response.status_code == 200
 
-    # def test_negative_update(self):
-    #     response = client.put(
-    #         f"{settings.API_PREFIX}{local_prefix}{self.wrong_id}",
-    #         json={"slug": fake_slug_2,
-    #               "name": fake_name,
-    #               "category_id": self.category_id,
-    #               "insurance_type_id": self.insurance_type_id,
-    #               "detail_id": self.detail_id,
-    #               "premium_type": fake_premium_type},
-    #     )
-    #     assert response.status_code == 404, response.text
+    # ============ NEGATIVE TEST ============
+    def test_negative_get(self):
+        response = client.get(f"{settings.API_PREFIX}{local_prefix}" +
+                              f"{self.wrong_id}",
+                              headers=self.headers,)
+        assert response.status_code == 404
 
-    # def test_negative_delete(self):
-    #     response = client.delete(
-    #         f"{settings.API_PREFIX}{local_prefix}",
-    #         json={"id": self.wrong_id},
-    #     )
-    #     assert response.status_code == 404
+    def test_negative_update(self, request, setup_cache):
+        category_id = request.config.cache.get("c_category_id", None)
+        detail_id = request.config.cache.get("c_detail_id", None)
+        insurance_type_id = request.config.cache.get(
+            "c_insurance_type_id", None)
+
+        response = client.put(
+            f"{settings.API_PREFIX}{local_prefix}{self.wrong_id}",
+            json={"slug": fake_slug_2,
+                  "name": fake_name,
+                  "category_id": category_id,
+                  "insurance_type_id": insurance_type_id,
+                  "detail_id": detail_id,
+                  "premium_type": fake_premium_type},
+        )
+        assert response.status_code == 404, response.text
+
+    def test_negative_delete(self):
+        response = client.delete(
+            f"{settings.API_PREFIX}{local_prefix}",
+            json={"id": self.wrong_id},
+        )
+        assert response.status_code == 404
